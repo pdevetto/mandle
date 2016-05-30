52a802aa17653a5540a82e59b2d9fce261013b08
Pierre <pierre.de-vettor@liris.cnrs.fr>
2016-05-30 15:00:13 +0300

    reorganization

--- mandle/mandle2.py
+++ mandle/mandle2.py
@@ -1,25 +1,22 @@
 # coding: utf8
-
 import threading, os, webbrowser
 import xml.etree.ElementTree as ET
-import BaseHTTPServer, SimpleHTTPServer, urlparse
+import urllib, BaseHTTPServer, SimpleHTTPServer, urlparse
+import Levenshtein
+# Templating
 from jinja2 import Environment, FileSystemLoader
-#import Levenshtein
+
+import pidevs, movies
 
 PORT = 8080
 DIRE = "/home/pdevetto/Misc/TOKEY"
 DIRE = "/drives/m/Films/"
 DOMA = "http://localhost:%s/" % (PORT)
 
-def cleantext(t): 
-    m = t.encode("windows-1252", "ignore")
-    m = m.decode("utf8", "ignore")
-    return m
-
 class TestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     content = ""
     title = "home"
-    
+
     def do_GET(self):
         """Handle a post request by returning the square of the number."""
         #length = int(self.headers.getheader('content-length'))
@@ -42,7 +39,7 @@ class TestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             s = urlparse.parse_qs(parsed.query)['s']
             self.title = "search:" + s[0]
             self.content += M.search(s[0])
-        else:            
+        else:
             self.content += M.getAll()
         return self.view()
 
@@ -50,127 +47,14 @@ class TestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        
+
         env = Environment(loader=FileSystemLoader('%s' % os.path.dirname(__file__)))
         template = env.get_template('index.htm')
         self.content = template.render(DOMA=DOMA, content=self.content, title=self.title)
-        
+
         self.content = cleantext(self.content)
         self.wfile.write(self.content)
         return
-
-def nospachar(t):
-    u = ""
-    for i in range(len(t)):
-        if 65 <= ord(t[i]) <= 90 or 97 <= ord(t[i]) <= 122:
-            u += t[i].lower()
-        elif t[i] in [" ", ".", ",", "-", "_", "/"]:
-            u += "_"
-    return u
-
-class Movies:
-    content = ""
-    def parcourir(self):
-        for root, dirs, files in os.walk(DIRE):
-            for curfile in files:
-                fi, fe = os.path.splitext(curfile)
-                if fe == ".nfo":
-                    yield os.path.join(root, curfile)
-
-    def getAll(self):
-        self.content = ""
-        i = 0
-        for mov in self.parcourir():
-            inf = self.nfoparse(mov)
-            if inf["year"] != None:
-                self.content += self.show(inf)
-                i += 1
-            if i == 50:
-                return self.content
-        return self.content
-
-    def search(self, sst):
-        self.content = ""
-        i = 0
-        for mov in self.parcourir():
-            inf = self.nfoparse(mov)
-            if sst.lower() in inf["title"].lower():
-                self.content += self.show(inf)
-                i += 1
-            if i == 50:    
-                return self.content
-        return "<h2> Search : " + sst + " </h2> " + self.content
-        
-    def getYear(self, year):
-        self.content = ""
-        for mov in self.parcourir():
-            inf = self.nfoparse(mov)
-            if inf["year"] == year:
-                self.content += self.show(inf)
-
-        return "<h2> Year : " + year + " </h2> " + self.content
-
-    def getReal(self, real):
-        self.content = ""
-        self.simi = ""
-        for mov in self.parcourir():
-            print mov
-            inf = self.nfoparse(mov)
-            if "director" in inf:
-                for di in inf["director"]:
-                    if nospachar(di) == real:
-                        self.content += self.show(inf)
-                    #elif Levenshtein.distance(cleantext(nospachar(di)), cleantext(real)) < 5:
-                    #    self.simi += " LEV " + cleantext(nospachar(di)) + " - " + cleantext(real) + " = " + Levenshtein.distance(cleantext(nospachar(di)), cleantext(real))
-                    #    self.simi += self.show(inf)
-                    else:
-                        pass
-        return "<h2> Real : " + real + " </h2> " + self.content # + " <hr> <h2> Similaires </h2> " + self.simi
-
-    def show(self, inf):
-        content = "<div class='movie' id='" + str(inf["id"]) + "'>" + "\r\n"
-        content += "  <img src='" + inf["img"] + "'/>" + "\r\n"
-        content += "  <span class='title'>" + inf["title"] + "</span><br>" + "\r\n"
-        if "director" in inf:
-            dis = []
-            for sho, di in inf["director"].items():
-                dis.append("<a href='" + DOMA + "real/" + sho + "'>" + di + "</a>")
-            content += "  <span class='real'>" + ",".join(dis) + "</span>" + "\r\n"
-        content += "<span class='year'><a href='" + DOMA + "year/" + str(inf["year"]) + "'>(" + str(inf["year"]) + ")</a></span>" + "\r\n"
-        content += "</div>" + "\r\n"
-        return content
-
-    def nfoparse(self, path):
-        data = {}
-        try:
-            tree = ET.parse(path)
-        except ET.ParseError:
-            self.content += "ERROR ON PARSE" + path
-            return data
-        root = tree.getroot()
-
-        movielt = root
-
-        data["title"] = movielt.find("title").text
-        data["note"] = movielt.find("rating").text
-        data["year"] = movielt.find("year").text
-        data["path"] = path
-        data["id"] = movielt.find("id").text
-
-        # DIrectores
-        real = movielt.findall("director")
-        if len(real) > 0 and real[0].text != None:
-            data["director"] = {}
-            for r in real:
-                sho = nospachar(r.text)
-                data["director"][sho] = r.text
-        # Image du film
-        th = movielt.findall("thumb")
-        if len(th) > 0:
-            data["img"] = th[0].text
-        else:
-            data["img"] = DOMA + "no.png"
-        return data
 
 def open_browser():
     """Start a browser after waiting for half a second."""
--- mandle/movies.py
+++ mandle/movies.py
@@ -1,26 +1,112 @@
-import urllib
+from pymongo import MongoClient
 
-def cleantext(t):
-    m = t.encode("windows-1252", "ignore")
-    m = m.decode("utf8", "ignore")
-    return m
-def clean(chaine):
-    return chaine.lower().strip()
-def decode(chaine):
-    chaine = chaine.replace(u"\u2018", "'").replace(u"\u2019", "'")
+class Movies:
+    content = ""
+    def parcourir(self):
+        for root, dirs, files in os.walk(DIRE):
+            for curfile in files:
+                fi, fe = os.path.splitext(curfile)
+                if fe == ".nfo":
+                    yield os.path.join(root, curfile)
+
+    def getAll(self):
+        self.content = ""
+        i = 0
+        for mov in self.parcourir():
+            inf = self.nfoparse(mov)
+            if inf["year"] != None:
+                self.content += self.show(inf)
+                i += 1
+            if i == 50:
+                return self.content
+        return self.content
+
+    def search(self, sst):
+        self.content = ""
+        i = 0
+        for mov in self.parcourir():
+            inf = self.nfoparse(mov)
+            if sst.lower() in inf["title"].lower():
+                self.content += self.show(inf)
+                i += 1
+            if i == 50:
+                return self.content
+        return "<h2> Search : " + sst + " </h2> " + self.content
+
+    def getYear(self, year):
+        self.content = ""
+        for mov in self.parcourir():
+            inf = self.nfoparse(mov)
+            if inf["year"] == year:
+                self.content += self.show(inf)
+
+        return "<h2> Year : " + year + " </h2> " + self.content
+
+    def getReal(self, real):
+        self.content = ""
+        self.simi = ""
+        for mov in self.parcourir():
+            print mov
+            inf = self.nfoparse(mov)
+            if "director" in inf:
+                for di in inf["director"]:
+                    if nospachar(di) == real:
+                        self.content += self.show(inf)
+                    #elif Levenshtein.distance(cleantext(nospachar(di)), cleantext(real)) < 5:
+                    #    self.simi += " LEV " + cleantext(nospachar(di)) + " - " + cleantext(real) + " = " + Levenshtein.distance(cleantext(nospachar(di)), cleantext(real))
+                    #    self.simi += self.show(inf)
+                    else:
+                        pass
+        return "<h2> Real : " + real + " </h2> " + self.content # + " <hr> <h2> Similaires </h2> " + self.simi
+
+    def show(self, inf):
+        content = "<div class='movie' id='" + str(inf["id"]) + "'>" + "\r\n"
+        content += "  <img src='" + inf["img"] + "'/>" + "\r\n"
+        content += "  <span class='title'>" + inf["title"] + "</span><br>" + "\r\n"
+        if "director" in inf:
+            dis = []
+            for sho, di in inf["director"].items():
+                dis.append("<a href='" + DOMA + "real/" + sho + "'>" + di + "</a>")
+            content += "  <span class='real'>" + ",".join(dis) + "</span>" + "\r\n"
+        content += "<span class='year'><a href='" + DOMA + "year/" + str(inf["year"]) + "'>(" + str(inf["year"]) + ")</a></span>" + "\r\n"
+        content += "</div>" + "\r\n"
+        return content
+
+#""""""""""""""""""
+# LIBS
+#""""""""""""""""""
+
+def nfoparse(self, path):
+    data = {}
     try:
-        chaine = unicodedata.normalize('NFKD', chaine).encode('ascii','ignore')
-        return chaine
-    except:
-        return chaine
-def remove_accents(input_str):
-    try:
-        nkfd_form = unicodedata.normalize('NFKD', unicode(input_str))
-        return u"".join([c for c in nkfd_form if not unicodedata.combining(c)])
-    except:
-        return input_str
-def cc(i):
-    return decode(clean(remove_accents(i)))
+        tree = ET.parse(path)
+    except ET.ParseError:
+        self.content += "ERROR ON PARSE" + path
+        return data
+    root = tree.getroot()
+
+    movielt = root
+
+    data["title"] = movielt.find("title").text
+    data["note"] = movielt.find("rating").text
+    data["year"] = movielt.find("year").text
+    data["path"] = path
+    data["id"] = movielt.find("id").text
+
+    # DIrectores
+    real = movielt.findall("director")
+    if len(real) > 0 and real[0].text != None:
+        data["director"] = {}
+        for r in real:
+            sho = nospachar(r.text)
+            data["director"][sho] = r.text
+    # Image du film
+    th = movielt.findall("thumb")
+    if len(th) > 0:
+        data["img"] = th[0].text
+    else:
+        data["img"] = DOMA + "no.png"
+    return data
 
 """
     Faire appel a themoviedb
